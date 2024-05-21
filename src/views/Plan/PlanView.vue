87accaeb1c990a57ec420eb1503c4ff1bbc247b6
강병헌 <fund88@naver.com>
2024-05-21 15:23:35 +0900

    feat : 다이어리

--- src/views/Plan/PlanView.vue
+++ src/views/Plan/PlanView.vue
@@ -1,23 +1,3 @@
-<script setup>
-import { ref } from 'vue';
-import FullCalendar from '@fullcalendar/vue3';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import DefaultNavbar from "@/examples/navbars/NavbarDefault.vue";
-import DefaultFooter from "@/examples/footers/FooterDefault.vue";
-import bgImage from "@/assets/img/bgg4.jpg";
-const backgroundStyle = {
-    backgroundImage: `url(${bgImage})`,
-};
-
-const calendarOptions = ref({
-    plugins: [dayGridPlugin],
-    initialView: 'dayGridMonth',
-    weekends: true,
-    events: [
-        { title: 'Meeting', start: new Date() }
-    ]
-});
-</script>
 <template>
     <div class="container position-sticky z-index-sticky top-0">
         <div class="row">
@@ -31,11 +11,13 @@ const calendarOptions = ref({
         </div>
     </div>
     <section>
-        <div class="page-header min-vh-100 " :style="backgroundStyle" loading="lazy">
+        <div class="page-header min-vh-100" :style="backgroundStyle" loading="lazy">
             <div class="row">
                 <div
                     class="col-4 offset-1 d-lg-flex d-none h-100 my-auto position-absolute top-3 start-0 text-center justify-content-center flex-column">
                     <div class="rounded-calendar">
+                        <!-- 조건부 렌더링을 통해 selectedDate가 있을 때만 ListDay 뷰 렌더링 -->
+                        <FullCalendar :options="listOptions" />
                     </div>
                 </div>
                 <div
@@ -49,15 +31,44 @@ const calendarOptions = ref({
     </section>
     <DefaultFooter />
 </template>
+
+<script setup>
+import { ref, computed } from 'vue';
+import FullCalendar from '@fullcalendar/vue3';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import listPlugin from '@fullcalendar/list';
+import DefaultNavbar from "@/examples/navbars/NavbarDefault.vue";
+import DefaultFooter from "@/examples/footers/FooterDefault.vue";
+import bgImage from "@/assets/img/bgg4.png";
+
+const backgroundStyle = {
+    backgroundImage: `url(${bgImage})`,
+};
+
+const selectedDate = ref(null);
+
+// Calendar options
+const calendarOptions = {
+    plugins: [dayGridPlugin, listPlugin],
+    initialView: 'dayGridMonth',
+    weekends: true,
+    events: [
+        { title: 'Meeting', start: new Date() }
+    ]
+};
+
+const listOptions = {
+    plugins: [listPlugin],
+    initialView: 'listDay',
+    events: calendarOptions.events
+};
+</script>
+
 <style scoped>
 .rounded-calendar {
     border-radius: 15px;
-    /* 모서리 둥글게 */
     padding: 30px;
-    /* 커스텀 패딩 값 */
     background-color: #ffffff;
-    /* 배경색 흰색 */
     box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
-    /* 그림자 효과 추가 */
 }
 </style>
