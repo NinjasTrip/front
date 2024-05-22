a1e76f8ecc00db656846a7459d33e8aebcfbe045
seongddiyong <kodd1102@naver.com>
2024-05-22 17:44:00 +0900

    feat: board

---
+++ src/views/Board/BoardList.vue
@@ -0,0 +1,122 @@
+<template>
+    <div class="container position-sticky z-index-sticky top-0">
+        <div class="row">
+            <div class="col-12">
+                <NavbarDefault :sticky="true" />
+            </div>
+        </div>
+    </div>
+    <div class="page-header min-vh-75 d-flex align-items-center justify-content-center" :style="{ backgroundImage: `url(${bg0})` }">
+        <section class="translucent-section">
+            <div class="container">
+                <div class="row justify-content-center not-trans-section">
+                    <div class="col-lg-10">
+                        <h2 class="my-3 py-3 shadow-sm text-center">
+                            <mark class="sky">글목록</mark>
+                        </h2>
+                    </div>
+                    <div class="col-lg-10">
+                        <div class="row mb-2">
+                            <div class="col-md-2 text-start">
+                                <button type="button" class="btn btn-outline-primary btn-sm" @click="moveWrite">글쓰기</button>
+                            </div>
+                            <div class="col-md-5 offset-md-5">
+                                <form class="d-flex">
+                                    <VSelect :selectOption="selectOption" @onKeySelect="changeKey" />
+                                    <div class="input-group input-group-sm ms-1">
+                                        <input type="text" class="form-control" v-model="param.word" placeholder="검색어..." />
+                                        <button class="btn btn-dark" type="button" @click="getArticleList">검색</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <BoardListItem v-for="article in articles" :key="article.boardIdx" :article="article" />
+                        </div>
+                    </div>
+                    <BoardPagination />
+                </div>
+            </div>
+        </section>
+    </div>
+    <FooterDefault />
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import { listArticle } from "@/api/board.js";
+
+import VSelect from "../common/VSelect.vue";
+import BoardListItem from "./item/BoardListItem.vue";
+import NavbarDefault from "@/examples/navbars/NavbarDefault.vue";
+import FooterDefault from "@/examples/footers/FooterDefault.vue";
+import BoardPagination from "@/views/Board/item/BoardPagination.vue";
+import { storeToRefs } from "pinia";
+import bg0 from "@/assets/img/bgg6.jpg";
+
+const router = useRouter();
+
+const selectOption = ref([
+    { text: "검색조건", value: "" },
+    { text: "글번호", value: "article_no" },
+    { text: "제목", value: "subject" },
+    { text: "작성자아이디", value: "user_id" },
+]);
+
+const articles = ref();
+const currentPage = ref(1);
+const totalPage = ref(0);
+const param = ref({
+    pgno: currentPage.value,
+    spp: VITE_ARTICLE_LIST_SIZE,
+    key: "",
+    word: "",
+});
+
+onMounted(() => {
+    getArticleList();
+});
+
+const changeKey = (val) => {
+    param.value.key = val;
+};
+
+const getArticleList = () => {
+    listArticle(
+        param.value,
+        ({ data }) => {
+            articles.value = data.articles;
+            console.log(articles.value);
+            // currentPage.value = data.currentPage;
+            // totalPage.value = data.totalPageCount;
+        },
+        (error) => {
+            console.error(error);
+        }
+    );
+};
+
+const onPageChange = (val) => {
+    currentPage.value = val;
+    param.value.pgno = val;
+    getArticleList();
+};
+
+const moveWrite = () => {
+    router.push({ name: "article-write" });
+};
+</script>
+
+<style scoped>
+.translucent-section {
+    background-color: rgba(255, 255, 255, 0.8); /* Adjust the opacity as needed */
+    padding: 20px; /* Add padding to prevent content from touching the edges */
+    border-radius: 10px; /* Optional: add rounded corners */
+}
+
+.not-trans-section {
+    background-color: rgba(255, 255, 255); /* Adjust the opacity as needed */
+    border-radius: 10px; /* Optional: add rounded corners */
+}
+</style>
---
+++ src/views/Board/item/BoardListItem.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({ article: Object });
+</script>
+
+<template>
+    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+        <div class="card h-100">
+            <img :src="article.imageUrl" class="card-img-top" alt="대표 사진" />
+            <div class="card-body">
+                <h5 class="card-title">{{ article.subject }}</h5>
+                <router-link :to="{ name: 'article-view', params: { boardIdx: article.boardIdx } }" class="btn btn-primary"> 자세히 보기 </router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card-img-top {
+    height: 200px;
+    object-fit: cover;
+}
+</style>
